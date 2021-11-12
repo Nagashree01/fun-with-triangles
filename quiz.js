453eef5ad4198bec28c53f44b2c7a19fa286a3c0
Nagashree01 <nagahegde2001@gmail.com>
2021-11-12 17:37:25 +0530

    fix: validating input

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ const quizForm = document.querySelector(".quiz-form");
 const submitButton = document.querySelector("#submit-answer-btn");
 const output = document.querySelector("#output");
 
-const correctAnswers = ["90°", "right angled", "one right angle"]
+const correctAnswers = ["90°", "right angled", "one right angle","a + b + c","no","45°"]
 
 function calculateScore() {
     let score = 0;
